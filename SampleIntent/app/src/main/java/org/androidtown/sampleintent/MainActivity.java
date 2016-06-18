afb6e1c8e1107d6e5686f51b881a1ae6e95bd840
Minki Choi <choiminki90@gmail.com>
2016-06-18 20:35:35 +0900

    add my modified files

--- SampleIntent/app/src/main/java/org/androidtown/sampleintent/MainActivity.java
+++ SampleIntent/app/src/main/java/org/androidtown/sampleintent/MainActivity.java
@@ -16,10 +16,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
     public void onButton3Clicked(View v) {
         Intent intent = new Intent(this,AnotherActivity.class);
         startActivityForResult(intent,REQUEST_CODE_ANOTHER_2);
     }
+
     public void onButton1Clicked(View v) {
         Intent intent = new Intent(getApplicationContext(),AnotherActivity.class);
         startActivityForResult(intent,REQUEST_CODE_ANOTHER);//인텐트 실행.
@@ -29,9 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
     //응답의 대한 결과를 실행시키는 메서드(onActivityResult)는 공통적으로 쓰이기 때문에
     //인텐트를 구분하는 리퀘스트값이 필요함.(어떤 인텐트에대한 응답인지 구분하기위해.)
+
     protected void onActivityResult(int requestCode,int resultCode,Intent intent) {
         super.onActivityResult(requestCode,resultCode,intent);
-
 
         Toast toast=Toast.makeText(getApplicationContext(),
                 "요청코드: "+requestCode+" 결과코드: "+resultCode,Toast.LENGTH_SHORT);
